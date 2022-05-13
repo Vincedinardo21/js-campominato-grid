d6ebfaf57fc50b10de1637ca0eea8d2dd2f30239
vince <vincedinardo21@gmail.com>
2022-05-13 16:23:59 +0200

    livello 1

---
+++ myscript.js
@@ -0,0 +1,23 @@
+let livello;
+let griglia = document.querySelector(".grid");
+
+// Inserisco il livello di difficoltà
+// Controllo che il livello sia da 1 - 3
+do{
+    livello = parseInt(prompt("Inserisci il livello di difficoltà da 1 a 3:"));
+}while(livello > 3);
+
+if(livello === 1){
+    for(let i = 0; i < 100; i++){
+        let divEl = createMyElement();
+        griglia.append(divEl);
+    }
+}
+
+// Funzioni
+function createMyElement(){
+    let node = document.createElement("div");
+    node.className = "square";
+    return node;
+}
+
