510499f6f78e4d70ab10a1240e6e4f3c8c28866d
vince <vincedinardo21@gmail.com>
2022-05-13 16:26:02 +0200

    Fix condizione dei livelli

--- myscript.js
+++ myscript.js
@@ -3,9 +3,9 @@ let griglia = document.querySelector(".grid");
 
 // Inserisco il livello di difficoltà
 // Controllo che il livello sia da 1 - 3
-do{
+do {
     livello = parseInt(prompt("Inserisci il livello di difficoltà da 1 a 3:"));
-}while(livello > 3);
+} while(livello > 3 || livello <= 0);
 
 if(livello === 1){
     for(let i = 0; i < 100; i++){
